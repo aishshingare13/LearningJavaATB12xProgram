398691b19e4006f85b4386b96e22ad1236b16659
Aishwarya9813 <shingareaish13@gmail.com>
2025-06-21 13:12:20 +0530

    Multiplication Table Program

---
+++ src/Coding_Challeange/MultiplicationTablePrg.java
@@ -0,0 +1,14 @@
+package Coding_Challeange;
+
+public class MultiplicationTablePrg {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Table of " + i + ":");
+
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            System.out.println();
+        }
+    }
+}
