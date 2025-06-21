02a1f5bee7afb282f3d18a293ff61db118ff2703
Aishwarya9813 <shingareaish13@gmail.com>
2025-06-21 11:56:11 +0530

    Variable Declaration & Initialization  Program

---
+++ src/Coding_Challeange/VariableDeclarationInitialization.java
@@ -0,0 +1,16 @@
+package Coding_Challeange;
+
+public class VariableDeclarationInitialization {
+    public static void main(String[] args) {
+
+        int i = 25;
+        double d = 3.14;
+        boolean b = true;
+        char c = 'A';
+
+        System.out.println("Integer: " + i);
+        System.out.println("Double:" + d);
+        System.out.println("Boolean: " + b);
+        System.out.println("Character: " + c);
+    }
+}
