2552172e29045189dacd2759f62f7bdffd8c81a9
Aishwarya9813 <shingareaish13@gmail.com>
2025-06-21 12:38:08 +0530

    Number Classification Program

---
+++ src/Coding_Challeange/NumberClassification.java
@@ -0,0 +1,23 @@
+package Coding_Challeange;
+
+import java.util.Scanner;
+
+public class NumberClassification {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number > 0) {
+            System.out.println(number + " is positive");
+        } else if (number < 0) {
+            System.out.println(number + " is negative");
+        } else {
+            System.out.println(number + " is zero");
+        }
+
+        scanner.close();
+
+    }
+}
