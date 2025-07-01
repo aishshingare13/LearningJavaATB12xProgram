b6d345dc06a9b412b0159429689160f6d57eae65
Aishwarya9813 <shingareaish13@gmail.com>
2025-07-01 22:11:30 +0530

    Collection Framework Ser Programs

---
+++ src/ex_Lab27_Collection_Framework/Set/Lab251_Set.java
@@ -0,0 +1,21 @@
+package ex_Lab27_Collection_Framework.Set;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Lab251_Set {
+    public static void main(String[] args) {
+        Set hs = new HashSet();
+        Set lhs = new LinkedHashSet();
+        Set ts = new TreeSet();
+
+        hs.add("Pramod");
+        hs.add("Pramod");
+        hs.add("pramod");
+        hs.add("dramod");
+        System.out.println(hs);
+
+    }
+}
